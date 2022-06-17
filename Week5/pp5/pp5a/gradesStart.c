9d1c46b5069f3dde4a744d5476c03fcc19366ca8
BuildTools <unconfigured@null.spigotmc.org>
2022-06-17 07:51:15 -0500

    pp5a done

---
+++ Week5/pp5/pp5a/gradesStart.c
@@ -0,0 +1,47 @@
+/* File: grades.c
+ * Author: Cindy Arnold
+ * Description: demonstrates dynamically allocates array
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+   int count, i;
+   // ADD CODE HERE TO declare pointer to grades (doubles)
+   double *grades;
+
+   double sum = 0, average = 0;
+   
+   // as user how many grades
+   printf( "How many grades?  " );
+   scanf( "%d", &count );
+   
+   // ADD CODE HEE TO dynamically allocate count grades
+   grades = (double*) malloc(sizeof(double)*count);
+   
+   // ADD CODE HERE TO get the grades
+   i = 0;
+
+   printf("Enter in your grades now, seperated by an enter:\n"); //Give user instructions for entering grades
+
+   while (i < count) { //User populates grades array until it is full
+      scanf( "%lf", &grades[i]);
+      i++;
+   }
+
+
+   // ADD CODE HERE TO compute and print the sum and average
+  
+   for (i = 0; i < count; i++) { //Calculate sum
+      sum += grades[i];
+   }
+
+   average = (sum /(double)count); //Calculate average
+
+   //printf("Sum of all grades: %0.2lf\n", sum);
+   printf("Average of all grades: %0.2lf\n", average); //Print average
+
+
+   free(grades); //Deallocate dynamic memory to prevent memory leaks
+   return 0;
+}
