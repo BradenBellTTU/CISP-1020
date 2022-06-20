9d5fe29228066f76d285af64640059f9c4e6de02
BuildTools <unconfigured@null.spigotmc.org>
2022-06-20 07:33:51 -0500

    asdf

--- Week5/pp5/pp5b/dbl_linked_list.h
+++ Week5/pp5/pp5b/dbl_linked_list.h
@@ -1,6 +1,8 @@
 #ifndef DBL_LINKED_LIST_H
 #define DBL_LINKED_LIST_H
 
+#include "node.h"
+
 typedef struct {
     node_t* headPtr;
     node_t* tailPtr;
@@ -8,7 +10,7 @@ typedef struct {
 } dbl_linked_list_t;
 
 void createList(dbl_linked_list_t*);
-void insertNode(dbl_linked_list_t*, node_t);
+void insertNode(dbl_linked_list_t*, node_t*);
 node_t* popNode(dbl_linked_list_t*);
 node_t* dequeueNode(dbl_linked_list_t*);
 void deleteList(dbl_linked_list_t*);
