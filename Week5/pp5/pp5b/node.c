74e722ab7f0780abf1ee20ad18e8ef447201dba3
BuildTools <unconfigured@null.spigotmc.org>
2022-06-20 17:47:35 -0500

    coments

--- Week5/pp5/pp5b/node.c
+++ Week5/pp5/pp5b/node.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include "node.h"
 
+/* initNode: initializes a node set up for a doubly linked list
+ * Parameters:
+ *    i = the data to be stored in the node
+ * Returns: a pointer to the node
+ */
 node_t* initNode(int data) {
     node_t* nodePtr;
     nodePtr = malloc(sizeof(node_t));
