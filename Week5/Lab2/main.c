1a2e6e5afc4ef58f917049c42b05dc3d16cda61f
BuildTools <unconfigured@null.spigotmc.org>
2022-06-23 14:50:34 -0500

    idontknowanymore

--- Week5/Lab2/main.c
+++ Week5/Lab2/main.c
@@ -3,8 +3,14 @@
 #include "list.h"
 #include "node.h"
 #include "stock.h"
+#include "date.h"
 
 const int MAX_STR_LEN = 256;
+
+void report();
+void buy();
+int sell();
+void printBin();
 
 int main() {
     int usrMenuSelect = 0;
@@ -54,36 +60,121 @@ void report() {
 
     char tempString[MAX_STR_LEN];
 
+
     while( (dirEntry = readdir( dirPtr )) != NULL) {
         if ( strstr(dirEntry->d_name, ".bin") != NULL) { //Check for .bin files
             strncpy(tempString, dirEntry->d_name, strlen(dirEntry->d_name) - 4); //remove .bin and store it in tempString
+            //printf("Stocks Owned\n--------\n");
             printf("%s\n", tempString); //Print file names to console
+            //read that bin and count total and print total
+            printBin(tempString);
+
+
         }
     }
 
     closedir(dirPtr);
-
+    //read what they pick and print the stocks from that file
 }
 
 
 void buy() {
-    char tickerSymbol[MAX_STR_LEN];
-    int numShares = 0;
-    float stockPrice = 0;
+    stock_t s;
     FILE* outFileStream;
 
     printf("Enter stock ticker symbol: ");
-    scanf("%s", tickerSymbol);
+    scanf("%s", s.ticker);
     printf("Enter number of shares: ");
-    scanf("%d", numShares);
-    printf("%f", stockPrice);
+    scanf("%d", &s.numShares);
+    printf("%f", s.pricePerShare);
+    setToday(s.date);
 
-    outFileStream = fopen(tickerSymbol, "a");
+
+    outFileStream = fopen(s.ticker, "a");
     
-    //?????
+    
+    //fwrite to write s
+    
 
-
-    printf("%d shares of %s purchased at $%f per share.\n", numShares, tickerSymbol, stockPrice);
+    printf("%d shares of %s purchased at $%f per share.\n", s.numShares, s.ticker, s.pricePerShare);
     fclose(outFileStream);
     
+}
+
+int sell() {
+    dbl_linked_list_t l;
+    char tickerSymbol[256];
+    FILE* inFileStream;
+    int desiredStocks = 0, total = 0;
+
+
+    scanf("%s", tickerSymbol);//get ticker symbol: eg APPL
+    toUpperCase(tickerSymbol); //uppercase symbol and 
+    strcat(tickerSymbol, ".bin"); //strcat ".bin" to the end of it
+    
+    
+    inFileStream = fopen(tickerSymbol, "rb");//open that file
+    if (inFileStream == NULL) {//if file open failed
+        printf("Error: File does not exist.\n");//error message
+        return 0;
+    }
+    //total = read from file, count total, and fill up list l (FILE* in, &dbl_linked_list l)
+    //print total and 
+    printf("Enter number of shares: "); //get how many user wants
+    scanf("%d", desiredStocks);
+
+
+    if (desiredStocks > total) {//if they want too many then return an error message and return to main menu
+        printf("Error: not enough stocks to sell\n");
+        return 0;
+    }
+
+    //desiredStocks < #in node
+        //decriment # in the node
+        //done
+
+    //if desiredStocks == #in the node
+        //pop node
+        //deallocate the node
+        //done
+
+    //if desiredStocks > #in the node
+        //decriment and pop if needed
+
+    //if the list isn't empty
+        //write list back to file - put that function in list files
+        //deleteList(&l);
+    //else
+        //remove(filename);
+}
+
+
+//Need help!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+void printBin(char* fileNameIn) {
+    char fileName[256];
+    strcpy(fileName, fileNameIn);
+    strcat(fileName, ".bin");
+    FILE* inFileStream;
+
+    stock_t testVar;
+    inFileStream = fopen(fileName, "rb");
+    if (inFileStream != NULL) {
+        fread(&testVar, sizeof(stock_t), 1, inFileStream);
+        fclose(inFileStream);
+    }
+
+    
+    printf("\t%d\n", testVar.numShares);
+    
+    
+    
+
+}
+
+
+
+void toUpperCase(char s[]) {
+	for (int i = 0; s[i] != '\0'; i++ ) {
+		s[i] = toupper(s[i]);
+	}
 }
