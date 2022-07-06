d625acdb56cc4576635ea6257600d9e66e2ab662
BradenBellTTU <babell45@tntech.edu>
2022-07-06 08:29:42 -0500

    pp9a done pp9b start

--- Week9/pp9a/myAlgorithms.h
+++ Week9/pp9a/myAlgorithms.h
@@ -2,7 +2,14 @@
 #define MYALGORITHMS_H
 
 
-
+/* mySwap: swaps the first item with the second item
+Parameters:
+	item1 - the first item
+	item2 - the second item
+Pre-condition(s):
+	type T has defined operator=
+Returns: void
+*/
 template<class T>
 void mySwap(T& item1, T& item2) {
 	T temp;
@@ -11,7 +18,14 @@ void mySwap(T& item1, T& item2) {
 	item2 = temp;
 }
 
-
+/* myMin: Compares 2 values and returns the minimum
+Parameters:
+	item1 - the first item to be compared
+	item2 - the second item to be compared
+Pre-condition(s):
+	type T has defined operator<
+Returns: T The item that is the least of the two
+*/
 template<class T>
 T myMin(T& item1, T& item2) {
 	if (item1 < item2)
@@ -19,6 +33,15 @@ T myMin(T& item1, T& item2) {
 	return item2;
 }
 
+
+/* mySort: Sorts an array from least to greatest
+Parameters:
+	array - the array
+	n - the number of valid elements in the array
+Pre-condition(s):
+	type T has defined operator<
+Returns: void
+*/
 template<class T>
 void mySort(T array[], int n) {
 	int min = 0;
@@ -31,12 +54,20 @@ void mySort(T array[], int n) {
 	}
 }
 
-
+/* mySearch: searches for a key in an array
+Parameters:
+	array - the array
+	n - the number of valid elements in the array
+	key - the key to look for
+Pre-condition(s):
+	type T has defined operator==
+Returns: int index where key is found or -1 if not found
+*/
 template<class T>
 int mySearch(T array[], int n, T key) {
 	for (int i = 0; i < n; i++) {
 		if (array[i] == key) {
-			return 1;
+			return i;
 		}
 	}
 	return -1;
