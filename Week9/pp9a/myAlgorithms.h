3dee72ce860f9163f0396007a5bf0261c5fddc19
BradenBellTTU <babell45@tntech.edu>
2022-07-06 07:41:03 -0500

    pp9a init

---
+++ Week9/pp9a/myAlgorithms.h
@@ -0,0 +1,48 @@
+#ifndef MYALGORITHMS_H
+#define MYALGORITHMS_H
+
+
+
+template<class T>
+void mySwap(T& item1, T& item2) {
+	T temp;
+	temp = item1;
+	item1 = item2;
+	item2 = temp;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
