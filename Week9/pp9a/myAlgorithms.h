be27971f85b3cd6d46d42882f6ce622fd7ef5116
BradenBellTTU <babell45@tntech.edu>
2022-07-06 08:07:17 -0500

    pp9a done

--- Week9/pp9a/myAlgorithms.h
+++ Week9/pp9a/myAlgorithms.h
@@ -12,7 +12,35 @@ void mySwap(T& item1, T& item2) {
 }
 
 
+template<class T>
+T myMin(T& item1, T& item2) {
+	if (item1 < item2)
+		return item1;
+	return item2;
+}
 
+template<class T>
+void mySort(T array[], int n) {
+	int min = 0;
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (array[j] < array[i]) {
+				mySwap(array[j], array[i]);
+			}
+		}
+	}
+}
+
+
+template<class T>
+int mySearch(T array[], int n, T key) {
+	for (int i = 0; i < n; i++) {
+		if (array[i] == key) {
+			return 1;
+		}
+	}
+	return -1;
+}
 
 
 
